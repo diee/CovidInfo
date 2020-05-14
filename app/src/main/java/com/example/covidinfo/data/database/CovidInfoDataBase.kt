47f9cc2d3bdadaa3edd2484b6c71ad9a2aa95b8e
diegoalarcon <die.r.alarcon@gmail.com>
2020-05-14 19:37:32 -0300

    Changed Dagger by Koin

--- app/src/main/java/com/example/covidinfo/data/database/CovidInfoDataBase.kt
+++ app/src/main/java/com/example/covidinfo/data/database/CovidInfoDataBase.kt
@@ -1,10 +1,20 @@
 package com.example.covidinfo.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [CountryCasesEntity::class], version = 1)
 abstract class CovidInfoDataBase : RoomDatabase(){
+
+    companion object {
+        fun build(context: Context) = Room.databaseBuilder(
+            context,
+            CovidInfoDataBase::class.java,
+            "covidinfo-db"
+        ).build()
+    }
 
     abstract fun countryCasesDao(): CountryCasesDao
 }
